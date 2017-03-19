d293c785149f357194dfd74c8947ae4064af318e
JerryZ <jerryzhu99@gmail.com>
2017-03-19 14:06:41 -0700

    Minor changes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var scope;
 angular.module("app", [])
 .controller("editor",function($scope, $http){
     scope = $scope;
-    $http.get('questions.json').then(function(data, status, headers, config) {
+
+    /*$http.get('questions.json').then(function(data, status, headers, config) {
         $scope.questions = data.data;
         $http.get('studies.json').then(function(data, status, headers, config) {
             $scope.studies = data.data;
@@ -17,8 +18,21 @@ angular.module("app", [])
         });
     }).catch(function(data, status, headers, config) {
         console.log(data);
+    });*/
+    Promise.all([$http.get('questions.json'),$http.get('studies.json')]).then(function(response){
+        console.log(response);
+        $scope.questions = response[0].data;
+        $scope.studies = response[1].data;
+        var s = localStorage.getItem('selected');
+        if(s){
+            $scope.studies = JSON.parse(s);
+        }
+        $scope.size = $scope.studies.length;
+        $scope.update();
+        $scope.$apply();
+    }).catch(function(response){
+        console.log(response);
     });
-
     $scope.update = function(){
         for(var i=0;i<$scope.questions.length;i++){
             var question = $scope.questions[i];
@@ -43,6 +57,7 @@ angular.module("app", [])
         }
         localStorage.setItem('selected', JSON.stringify($scope.studies));
     }
+
     $scope.highlight = function(study, value){
         for(var i=0;i<$scope.questions.length;i++){
             var question = $scope.questions[i];
